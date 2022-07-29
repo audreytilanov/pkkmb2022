1c4e81e8f2a02cbeb03896804f074ae577d302ac
audreytilanov <tilanovaudrey@gmail.com>
2022-07-29 16:21:08 +0800

    add halaman di ukm

--- app/Http/Controllers/UkmController.php
+++ app/Http/Controllers/UkmController.php
@@ -9,16 +9,19 @@ use App\Models\prestasi;
 class UkmController extends Controller
 {
     public function index(){
+        $halaman = "lembaga";
         return view('page.ukm', [
             "title" => "UKM",
             "ukm"=> ukm::all()
-        ]);
+        ],compact('halaman'));
     }
 
     public function show(Ukm $detail){
+        $halaman = "lembaga";
+
         return view('page.ormawa.detail-ukm',[
             "title"=> "Detail UKM",
             "ukm" => $detail
-        ]);
+        ], compact('halaman'));
     }
 }
